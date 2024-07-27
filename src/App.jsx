68f3fc77a092be8056dd007cecfcbc57f8477617
Vinay Singirikonda <kingking148414@gmail.com>
2024-07-27 12:38:51 +0530

    add broswer router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signup from "./components/Signup";
 import Signin from "./components/Signin";
 import Home from "./components/Home";
@@ -7,18 +8,25 @@ import History from "./components/History";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 function App() {
-  const router = createBrowserRouter([
-    { path: "/signup", element: <Signup /> },
-    { path: "/signin", element: <Signin /> },
-    { path: "/history", element: <History /> },
-    { path: "/", element: <Home /> },
-  ]);
+  // const router = createBrowserRouter([
+  //   { path: "/signup", element: <Signup /> },
+  //   { path: "/signin", element: <Signin /> },
+  //   { path: "/history", element: <History /> },
+  //   { path: "/", element: <Home /> },
+  // ]);
 
   const cliendId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
   return (
     <GoogleOAuthProvider clientId={cliendId}>
-      <RouterProvider router={router} />
+      <Router>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/history" element={<History />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </GoogleOAuthProvider>
   );
 }
